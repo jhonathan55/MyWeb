9f64b9c8962868895c5cf4c75c2a3c784fd31385
jhonathan55 <jfgutierrezsaico@gmail.com>
2022-08-09 19:05:19 -0400

    card

--- src/app/pages/card/card.component.ts
+++ src/app/pages/card/card.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -8,16 +8,51 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class CardComponent implements OnInit {
   private isValidEmail = /\S+@\S+\.\S+/;
-
+  public colSize = 3;
+  public rowHeight = '2:3';
+  public isMobile: boolean = false;
+  public isTable: boolean = false;
   constructor(
-    private fb: FormBuilder
-  ) { }
+    private fb: FormBuilder,
+    breakpointObserver: BreakpointObserver
+  ) {
+
+    breakpointObserver.observe([
+      Breakpoints.Handset,
+    ]).subscribe(result => {
+      if (result.matches) {
+        console.log(result.matches);
+        
+        this.isMobile = true;
+        this.colSize = 1;
+        this.rowHeight = '550px';
+      } else {
+        this.isMobile = false;
+        this.colSize = 3;
+        this.rowHeight = '2:3';
+      }
+
+      /* if (this.isMobile) {
+         this.colSize=1;
+         this.rowHeight='550px';
+       }else if(this.isTable){
+         this.colSize=3;
+         this.rowHeight='700px';
+       } 
+       else {
+         this.colSize=3;
+         this.rowHeight = '2:3';
+       }*/
+    }
+    );
+  }
 
   contacFrm = this.fb.group({
     name: ['', [Validators.required]],
     email: ['', [Validators.required, Validators.pattern(this.isValidEmail)]],
-    celular: ['', [Validators.required]],
-    message: ['', [Validators.required]]
+    phone: ['', [Validators.required]],
+   
+
   });
 
   ngOnInit(): void {
@@ -27,5 +62,9 @@ export class CardComponent implements OnInit {
   isValidField(field: string): string {
     const validatedField = this.contacFrm.get(field);
     return (!validatedField?.valid && validatedField?.touched) ? 'is-invalid' : validatedField?.touched ? 'is-valid' : '';
+  }
+
+  sendMessage(): void {
+    console.log(this.contacFrm.value);
   }
 }
